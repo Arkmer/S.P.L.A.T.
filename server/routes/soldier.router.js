9d0d3a6119c949e565a6b0f593db26072ceaa4a4
Ryan Udelhoven <Ryan@Udelhoven.net>
2018-03-23 08:31:13 -0500

    POST from Soliders tree is not working

--- server/routes/soldier.router.js
+++ server/routes/soldier.router.js
@@ -5,8 +5,8 @@ const pool = require('../modules/pool.js');
 
 router.post('/', (req, res) => {
     console.log('submitSoldier.POST', req.body);
-    pool.query('INSERT INTO soldier (first, last, rank, eval, ets, apft) VALUES ($1, $2, $3, $4, $5) RETURNING id',
-      [req.body.first, req.body.last, req.body.rank, req.body.eval, req.body.ets, req.body.apft], (err, result) => {
+    pool.query('INSERT INTO soldier (first, last, rank, eval, ets, apft, unit_id) VALUES ($1, $2, $3, $4, $5, $6, $7);',
+      [req.body.first, req.body.last, req.body.rank, req.body.eval, req.body.ets, req.body.apft, req.body.unit], (err, result) => {
         if (err) {
           console.log("Error inserting data: ", err);
           res.sendStatus(500);
