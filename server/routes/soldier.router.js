71b2757ef004b41b0aeb2d189cf5fa040c1d021f
Ryan Udelhoven <Ryan@Udelhoven.net>
2018-03-23 15:56:50 -0500

    minor auth update

--- server/routes/soldier.router.js
+++ server/routes/soldier.router.js
@@ -1,31 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require('body-parser');
-const pool = require('../modules/pool.js');
 
 router.post('/', (req, res) => {
-    console.log('submitSoldier.POST', req.body);
-    pool.query('INSERT INTO soldier (first, last, rank, eval, ets, apft, unit_id) VALUES ($1, $2, $3, $4, $5, $6, $7);',
-      [req.body.first, req.body.last, req.body.rank, req.body.eval, req.body.ets, req.body.apft, req.body.unit], (err, result) => {
+    if (req.isAuthenticated()) {
+        console.log('submitSoldier.POST', req.body);
+        pool.query('INSERT INTO soldier (first, last, rank, eval, ets, apft, unit_id) VALUES ($1, $2, $3, $4, $5, $6, $7);',
+        [req.body.first, req.body.last, req.body.rank, req.body.eval, req.body.ets, req.body.apft, req.body.unit], (err, result) => {
         if (err) {
-          console.log("Error inserting data: ", err);
-          res.sendStatus(500);
+            console.log("Error inserting data: ", err);
+            res.sendStatus(500);
         } else {
-          res.sendStatus(201);
+            res.sendStatus(201);
         }
-      })
-  });
+        })
+    }else{
+        res.sendStatus(403);
+    }
+});
 
 router.get('/:unit', (req, res)=>{
-    console.log('getSoldierRoster.GET');
-    let unit = req.params.unit;
-    pool.query(`select * from soldier where unit_id = ${unit};`)
-    .then(function(result) {
-        res.send(result.rows);
-    }).catch(function(error) {
-        console.log('there was a problem', error);
-        res.sendStatus(500);
-    })
+    if (req.isAuthenticated()) {
+        console.log('getSoldierRoster.GET');
+        let unit = req.params.unit;
+        pool.query(`select * from soldier where unit_id = ${unit};`)
+        .then(function(result) {
+            res.send(result.rows);
+        }).catch(function(error) {
+            console.log('there was a problem', error);
+            res.sendStatus(500);
+        })
+    }else{
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
